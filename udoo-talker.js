d0df0259adc153f9508d53506f65917432cff0fe
Iago Lastra <iago.lastra@quobis.com>
2016-02-28 12:02:44 +0100

    Add full sensor data

--- udoo-talker.js
+++ udoo-talker.js
@@ -2,13 +2,28 @@
 var udoo = require('udoo');
 var request = require('request');
 var neo = require("./udooneo");
+var SerialPort = require("serialport").SerialPort
+var lastDistance;
+
+var serialPort = new SerialPort("/dev/ttyMCC", {
+  baudrate: 115200
+});
+
+serialPort.on("open", function() {
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+    lastDistance = data;
+  });
+});
+
+
 
 var URL = '172.16.3.201/sensors/';
 
-gyroscopic = neo.sensors.Gyroscope;
-magnetometer = neo.sensors.Magnetometer;
-accelerometer = sensors.Accelerometer;
-real_path = "/sys/class/gpio/gpio174"
+var gyroscopic = neo.sensors.Gyroscope;
+var magnetometer = neo.sensors.Magnetometer;
+var accelerometer = sensors.Accelerometer;
+var real_path = "/sys/class/gpio/gpio174"
 
 function put(data, callback) {
   request({
@@ -19,30 +34,22 @@ function put(data, callback) {
 }
 
 function getSensorData() {
-  var data = [];
+  return [{
+    udoo_id: 1,
+    measure: gyroscopic.data(),
+  }, {
+    udoo_id: 2,
+    measure: magnetometer.data(),
+  }, {
+    udoo_id: 3,
+    measure: accelerometer.data(),
+  }, {
+    udoo_id: 4,
+    measure: lastDistance,
+  }];
 
-  // Get data from a sensor
-  gyroscopic_data = gyroscopic.data();
-  magnetometer = magnetometer.data();
-  accelerometer = accelerometer.data();
-  // var sensor_proximity = udoo.get("")
-
-  // Duplicar por cada sensor --------------------------
-  var sensorData = {
-    'gyroscopic': gyroscopic_data, // Fill this with real data
-    'magnetometer': magnetometer, // Fill this with real data
-    'accelerometer': accelerometer
-
-  };
-  data.push(sensorData);
-  //  --------------------------
-
-  return data;
-
-}
-
-// Main loop
-(function loop() {
-  put(getSensorData());
-  setTimeout(loop, 1000);
-}());
+  // Main loop
+  (function loop() {
+    put(getSensorData());
+    setTimeout(loop, 1000);
+  }());
